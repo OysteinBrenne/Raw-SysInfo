064a27717b59b6c94b8cde8e3bde59a6ae697f39
OysteinBrenne <oysteinbrenne@outlook.com>
2025-07-23 16:46:44 +0200

    0.8
    added cli option

--- modules/wmi_info.py
+++ modules/wmi_info.py
@@ -2,7 +2,7 @@ import wmi
 seperator = "-----------------------------------------------------------------------------------------------------"
 c = wmi.WMI()
 def get_wmi_info(input,name):
-    print (f"starting {name}")
+    #print (f"starting {name}")
     system_wmi_raw = input()
     system_wmi_str = f"{seperator}\n".join(str(nic) for nic in system_wmi_raw)
     if system_wmi_str == "":
@@ -16,6 +16,6 @@ def get_wmi_info(input,name):
         system_wmi_str = "\n".join(line.strip() for line in system_wmi_str.splitlines())
         system_wmi_str = "\n".join(filter(None, (line.strip() for line in system_wmi_str.splitlines())))
         system_wmi_str = system_wmi_str.strip()
-    print (f"closed {name}")
+    #print (f"closed {name}")
     return system_wmi_str
     
---
+++ pconfig.py
@@ -0,0 +1,80 @@
+import wmi
+from modules import wmi_info as wmii
+import sys
+c = wmi.WMI()
+
+
+#commands // sys,cpu,ram,disk,gpu,mb,bios,users,network,windows,battery,startup,audio,display,input
+
+cli_arg = str(sys.argv[1]).lower()
+if cli_arg == "sys":
+    print (wmii.get_wmi_info(c.Win32_ComputerSystem,"Win32_ComputerSystem"))
+elif cli_arg == "cpu":
+    print (wmii.get_wmi_info(c.Win32_Processor,"Win32_Processor"))
+
+elif cli_arg == "ram":
+    print(wmii.get_wmi_info(c.Win32_PhysicalMemory,"Win32_PhysicalMemory"))
+
+elif cli_arg == "disk":
+    print(wmii.get_wmi_info(c.Win32_DiskDrive,"Win32_DiskDrive"))
+
+elif cli_arg == "gpu":
+    print(wmii.get_wmi_info(c.Win32_VideoController,"Win32_VideoController"))
+
+elif cli_arg == "mb":
+    print(wmii.get_wmi_info(c.Win32_BaseBoard,"Win32_BaseBoard"))
+
+elif cli_arg == "bios":
+    print(wmii.get_wmi_info(c.Win32_BIOS,"Win32_BIOS"))
+
+elif cli_arg == "users":
+    print(wmii.get_wmi_info(c.Win32_UserAccount,"Win32_UserAccount"))
+
+elif cli_arg == "net":
+    print(wmii.get_wmi_info(c.Win32_NetworkAdapterConfiguration,"Win32_NetworkAdapterConfiguration"))
+
+elif cli_arg == "windows":
+    print(wmii.get_wmi_info(c.Win32_OperatingSystem,"Win32_OperatingSystem"))
+
+elif cli_arg == "battery":
+    print(wmii.get_wmi_info(c.Win32_Battery,"Win32_Battery"))
+
+elif cli_arg == "startup":
+    print(wmii.get_wmi_info(c.Win32_StartupCommand,"Win32_StartupCommand"))
+
+elif cli_arg == "audio":
+    print(wmii.get_wmi_info(c.Win32_SoundDevice,"Win32_SoundDevice"))
+
+elif cli_arg == "display":
+    print(wmii.get_wmi_info(c.Win32_DesktopMonitor,"Win32_DesktopMonitor"))
+
+elif cli_arg == "input":
+    print(wmii.get_wmi_info(c.Win32_Keyboard,"Win32_Keyboard"))
+    print(wmii.get_wmi_info(c.Win32_PointingDevice,"Win32_PointingDevice"))
+
+
+elif cli_arg == "help":
+    print("available commands")
+    print("--------------------------------------")
+
+    print("sys")
+    print("cpu")
+    print("ram")
+    print("disk")
+    print("gpu")
+    print("mb")
+    print("bios")
+    print("users")
+    print("net")
+    print("windows")
+    print("battery")
+    print("startup")
+    print("audio")
+    print("display")
+    print("input")
+
+
+
+else: 
+    print (f"{cli_arg} is not a valid argument, use help to list avalible arguments")
+print ()
